f96cfa7f7a4915ac3e50a289b41fef8f7af850fe
arthurgnoronha <gattermannoronha@gmail.com>
2025-04-01 15:00:34 -0300

    Divisão de Microsserviços

---
+++ salaApplication/src/main/java/com/sala/repository/SalaRepository.java
@@ -0,0 +1,6 @@
+package com.sala.sala.repository;
+
+import com.sala.sala.model.Sala;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalaRepository extends JpaRepository<Sala, Long> {}
---
+++ userApplication/src/main/java/com/usuario/controller/UsuarioController.java
@@ -0,0 +1,29 @@
+package com.usuario.usuario.controller;
+
+import com.usuario.usuario.model.Usuario;
+import com.usuario.usuario.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    @Autowired
+    private UsuarioService service;
+
+    @GetMapping
+    public String listar(Model model) {
+        model.addAttribute("usuarios", service.listar());
+        return "usuarios";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Usuario usuario) {
+        service.salvar(usuario);
+        return "redirect:/usuarios";
+    }
+}
